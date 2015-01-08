d337dd442d18b46c2ccd52659292534b889d57fb
borislav <b.manolov@gmail.com>
2015-01-08 21:19:00 +0100

    return 404 if search word is not found

--- app/Controller/DefaultController.php
+++ app/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Cache(maxage="86400", smaxage="86400")
@@ -14,11 +15,16 @@ class DefaultController extends Controller {
 	 */
 	public function indexAction(Request $request) {
 		$searchTerm = $request->query->get('q');
-		return $this->render('App:Default:index.html.twig', array(
-			'words' => $this->em()->getWordRepository()->findByName($searchTerm),
+		$words = $this->em()->getWordRepository()->findByName($searchTerm);
+		$response = $this->render('App:Default:index.html.twig', array(
+			'words' => $words,
 			'searchTerm' => $searchTerm,
 			'letterCounts' => $this->em()->getStatValueRepository()->getLetterCounts(),
 		));
+		if (empty($words)) {
+			$response->setStatusCode(Response::HTTP_NOT_FOUND);
+		}
+		return $response;
 	}
 
 	/**
